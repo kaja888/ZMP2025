8731dc8cf176c29e3902f183cd7bc09da2400e0e
kaja888 <kajafeliniak@onet.pl>
2025-03-13 22:42:37 +0100

    Add files via upload

---
+++ 287359_z3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int l=0;
+	cout<<"podaj liczbe naturaln¹"<<endl;
+	cin>>n;
+	l=x/5;
+	cout<<"liczba zer w"<<x<<"! ="l<<endl;
+	return 0;
+}
