c07149c305161ecd31ff815c22b9667907c647a2
kaja888 <kajafeliniak@onet.pl>
2025-04-06 23:26:09 +0200

    Add files via upload

---
+++ L3/zad1/287359_z1a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+    int tab[2];
+    tab[0]=1;
+    tab[1]=1;
+    long double x;
+    cout<<"podaj ktory wyraz ciagu chcesz uzyskac"<<endl;
+    cin >> x;
+    long double y=0;
+
+    if(x<=2)
+	{
+      cout << 1;
+    }
+    else{
+      for(int i=0; i<x-1; i++)
+	  {
+        y=tab[0]+tab[1]; //zmienna pomocnicza.
+        tab[0]=tab[1];
+        tab[1]=y; 
+      }
+      cout << y << endl;
+    }
+   
+    return 0;
+}
